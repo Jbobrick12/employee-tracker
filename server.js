8a0fde3d4584981a193d207eac9cfc1b1db7dd36
Justin Bobrick <j.bobrick@outlook.com>
2023-09-08 00:22:14 -0700

    chopping through the server.js file

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const consoleTable = require('console.table');
+require("dotenv").config();
+const express = require('express');
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
 
+const app = express();
 const PORT = process.env.PORT || 3001;
 
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASSWORD;
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: dbUser,
+    password: dbPass,
+    database: dbName,
+  },
+  console.log("Connected Successfully!")
+);
